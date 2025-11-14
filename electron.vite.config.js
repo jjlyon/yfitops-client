848bf1d27a96dbe0fb589f1d7fc41e055967e66d
jjlyon <john.jlyonii@gmail.com>
2025-11-14 15:07:24 -0500

    Handle ESM electron-vite config and externalize formidable

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,46 +1,50 @@
 const { resolve } = require('path');
-const { defineConfig } = require('electron-vite');
-const vue = require('@vitejs/plugin-vue');
 
-module.exports = defineConfig({
-  main: {
-    build: {
-      outDir: resolve(__dirname, 'dist-electron'),
-      emptyOutDir: true,
-      rollupOptions: {
-        input: {
-          index: resolve(__dirname, 'src/main/index.js'),
-          spotifyService: resolve(__dirname, 'src/lib/spotifyService.js')
-        },
-        output: {
-          entryFileNames: (chunkInfo) => {
-            if (chunkInfo.name === 'index') {
-              return 'main/[name].js';
+module.exports = async () => {
+  const { defineConfig } = await import('electron-vite');
+  const { default: vue } = await import('@vitejs/plugin-vue');
+
+  return defineConfig({
+    main: {
+      build: {
+        outDir: resolve(__dirname, 'dist-electron'),
+        emptyOutDir: true,
+        rollupOptions: {
+          external: ['formidable'],
+          input: {
+            index: resolve(__dirname, 'src/main/index.js'),
+            spotifyService: resolve(__dirname, 'src/lib/spotifyService.js')
+          },
+          output: {
+            entryFileNames: (chunkInfo) => {
+              if (chunkInfo.name === 'index') {
+                return 'main/[name].js';
+              }
+
+              return 'lib/[name].js';
             }
-
-            return 'lib/[name].js';
           }
         }
       }
-    }
-  },
-  preload: {
-    build: {
-      outDir: resolve(__dirname, 'dist-electron/preload'),
-      emptyOutDir: true
-    }
-  },
-  renderer: {
-    root: resolve(__dirname, 'src/renderer'),
-    build: {
-      outDir: resolve(__dirname, 'dist-electron/renderer'),
-      emptyOutDir: true
     },
-    resolve: {
-      alias: {
-        '@renderer': resolve(__dirname, 'src/renderer')
+    preload: {
+      build: {
+        outDir: resolve(__dirname, 'dist-electron/preload'),
+        emptyOutDir: true
       }
     },
-    plugins: [vue()]
-  }
-});
+    renderer: {
+      root: resolve(__dirname, 'src/renderer'),
+      build: {
+        outDir: resolve(__dirname, 'dist-electron/renderer'),
+        emptyOutDir: true
+      },
+      resolve: {
+        alias: {
+          '@renderer': resolve(__dirname, 'src/renderer')
+        }
+      },
+      plugins: [vue()]
+    }
+  });
+};
